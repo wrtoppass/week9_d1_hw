6c6e9d946c941b98c9054b517385fff1f2ac68ce
William Toppass <34204695+wrtoppass@users.noreply.github.com>
2023-05-31 02:52:42 -0500

    Update App.tsx

--- car-inventory-dashboard/src/App.tsx
+++ car-inventory-dashboard/src/App.tsx
@@ -1,11 +1,13 @@
 import Car from './components/Car'
 import Header from './components/Header'
+import Dates from './components/Dates'
 
 function App() {
   return (
     <>
       <Car />
       <Header />
+      <Dates />
     </>
   )
 }
